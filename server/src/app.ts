b8ef3217b0d76840eb3866fad7deffb155eeeb94
Gitty Landau <GittyLandau04@gmail.com>
2025-06-18 18:59:25 -0400

    Fix Error

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Response, Request } from "express";
+import express, { Response, Request, NextFunction } from "express";
 import cors from "cors";
 import morgan from "morgan";
 import routes from "./routes/index";
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use("/api", routes);
 
 // Error handler
-app.use((err: any, _req: Request, res: Response) => {
+app.use((err: any, _req: Request, res: Response, _next: NextFunction) => {
   console.error("Error:", err);
   res.status(500).json({ error: "Internal server error" });
 });
